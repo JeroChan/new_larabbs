d597e6a43b98ae1032384456ec51c2b68bfc728e
JeroChan <446363041@qq.com>
2019-05-07 09:11:52 +0000

    用户管理后台

--- public/jero.php
+++ public/jero.php
@@ -1,1 +1,18 @@
-<?php phpinfo(); ?> 
+<?php
+
+echo '<pre>';
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue");
+$a2=array("a"=>"red","c"=>"blue","d"=>"pink");
+
+$result=array_diff_key($a1,$a2);
+print_r($result);
+
+
+$result=array_intersect_key($a1,$a2);
+print_r($result);
+
+
+
+
+?>
