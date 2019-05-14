1d648e93b1433d9a2215dd774e516fe699411676
JeroChan <446363041@qq.com>
2019-05-14 01:54:07 +0000

    提交代码

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,16 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
 ], function ($api) {
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => 1,
+        'expires' => 1,
+    ], function ($api) {
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
+
 });
