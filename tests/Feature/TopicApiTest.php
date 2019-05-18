b1d281f1fefe4559214cef76ee5fe89409c31224
JeroChan <446363041@qq.com>
2019-05-18 08:02:14 +0000

    修复问题

--- tests/Feature/TopicApiTest.php
+++ tests/Feature/TopicApiTest.php
@@ -68,4 +68,39 @@ class TopicApiTest extends TestCase
             'category_id' => 1,
         ]);
     }
+
+    public function testShowTopic()
+    {
+        $topic = $this->makeTopic();
+        $response = $this->json('GET', '/api/topics/'.$topic->id);
+
+        $assertData= [
+            'category_id' => $topic->category_id,
+            'user_id' => $topic->user_id,
+            'title' => $topic->title,
+            'body' => $topic->body,
+        ];
+
+        $response->assertStatus(200)
+            ->assertJsonFragment($assertData);
+    }
+
+    /*public function testIndexTopic()
+    {
+        $response = $this->json('GET', '/api/topics');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data', 'meta']);
+    }*/
+
+    public function testDeleteTopic()
+    {
+        $topic = $this->makeTopic();
+        $response = $this->JWTActingAs($this->user)
+            ->json('DELETE', '/api/topics/'.$topic->id);
+        $response->assertStatus(204);
+
+        $response = $this->json('GET', '/api/topics/'.$topic->id);
+        $response->assertStatus(404);
+    }
 }
